8103fad7ecede804ca28474455b08dbd7807dc32
AndrewTilt9 <T1506367@student.southstaffs.ac.uk>
2017-04-27 11:41:11 +0100

    Main update

--- main.js
+++ main.js
@@ -34,6 +34,12 @@ var mainStage ={
                         Phaser.Keyboard.SPACEBAR);
         spaceBar.onDownadd(this.jump,this);
         
+        //Create an empty group
+        this.pipes= game.add.group();
+        
+        //Thmer for pipes 
+        this.timer = game.time.event.loop(1500,this.addrowOfPipes,this);
+        
     },
     
     update: function() {
@@ -54,6 +60,39 @@ var mainStage ={
     restartGame: function() {
     //Start the 'main' tate, which restarts the game
     game.state.start('main');
+    },
+    
+    //Add a pipe
+    addOnePipe: function(x,y){
+        //Create a pipe at the position x and y 
+        var pipe = game.add.sprite(x, y, 'pipe');
+        
+        //Add pipe to group 
+        this.pipes.add(pipe); 
+        
+        //Enable the physiscs on the pipe 
+        game.physics.arcade.enable(pipe);
+        
+        //Add velocity to the pipe to make it move left 
+        pipe.body.velocity.x = -200;
+        
+        //Automatically kill pipe when it is no longer visible
+        pipe.checkworldBounds = true;
+        pipe.outOfBoundsKill = true;
+        
+    },
+    
+    //Many pipes 
+    addrowOfPipes: function() {
+    //randomly pick a number between 1 and 5 
+        //This will be the hole position in the pipe 
+        var hole= Math.Floor(math.random() *5) + 1;
+        
+        //Add 6 pipes 
+        for (var i= 0; i < 8 ; i++)
+            if (i !=hole && i !=hole+1)
+                this.addOnePipe(400, i * 60 +10);
+        
     },
 };
 
