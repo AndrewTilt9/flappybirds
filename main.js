67645a39131f018d3d246f5d273a9963a78c86d6
AndrewTilt9 <T1506367@student.southstaffs.ac.uk>
2017-04-27 11:18:33 +0100

    Main Changes

--- main.js
+++ main.js
@@ -3,14 +3,56 @@ var mainStage ={
     preload: function() {
     //This function will be exexcuted at the beginning
         //That's where we load the images and sound
+        
+        //Load the bird sprite
+        game.load.image('bird','assests/bird.png');
     },
+    
     create: function() {
     //This function is called after the preload function 
         //Here we setup the game,display sprites, etc.
+        
+        //Change the background colour of the game to blue - for now!
+        game.stage.backgroundColor = '#71c5cf';
+        
+        //Set the physics for the game 
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        //Display the bird at the position of x=100 and y=245
+        this.bird = game.add.sprite(100, 245, 'bird');
+        
+        //Add the physics to the bird 
+        //Needed for: movement,gravity, collisions, etc.
+        game.physics.arcade.enable(this.bird);
+        
+        //Add gravity to the bird to make it fall
+        this.bird.body.gravity.y = 1000;
+        
+        //Call 'jump' function when the spacebar is pressed 
+        var spaceBar = game.input.keyboard.addKey(
+                        Phaser.Keyboard.SPACEBAR);
+        spaceBar.onDownadd(this.jump,this);
+        
     },
+    
     update: function() {
     //This function is callde 60 times per second 
         //It contains the games logic 
+        
+        //call the 'restartGame' function
+        if (this.bird.y <0 || this.bird.y. 490)
+            this.restartGame();
+    },
+    
+    jump: function() {
+        //Add a vertical velocity to the bird
+        this.bird.body.velocity.y = -350;
+    },
+    
+    //Restart the game
+    restartGame: function() {
+    //Start the 'main' tate, which restarts the game
+    game.state.start('main');
     },
 };
 
@@ -22,3 +64,4 @@ game.state.add('main', mainState);
 
 //Start the state to actually start the game 
 game.state.start('main');
+
